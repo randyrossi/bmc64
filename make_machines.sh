2f1a6af46ac7e1c80bbc704a72b7c7d6d5804c4d
Randy Rossi <randy.rossi@gmail.com>
2019-06-03 19:12:31 -0400

    Fix script again

--- make_machines.sh
+++ make_machines.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
+
+if [ "$1" = "pi3" ]
+then
+KERNEL=kernel8-32.img
+elif [ "$1" = "pi2" ]
+then
+KERNEL=kernel7.img
+else
+echo "Need pi2 or pi3 arg"
+exit
+fi
+
 cd third_party/vice-3.3
 make x64
 make x128
 make xvic
 cd ../..
 
-if [ "$1" = "pi2" ]
-then
-KERNEL=kernel8-32.img
-else
-KERNEL=kernel7.img
-fi
-
 make clean
 make -f Makefile-C64
-echo cp $KERNEL ${KERNEL}.c64
+cp $KERNEL ${KERNEL}.c64
 
 make clean
 make -f Makefile-C128
-echo cp $KERNEL ${KERNEL}.c128
+cp $KERNEL ${KERNEL}.c128
 
 make clean
 make -f Makefile-VIC20
-echo cp $KERNEL ${KERNEL}.vic20
+cp $KERNEL ${KERNEL}.vic20
