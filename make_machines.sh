8afcdeeb40a9db943eba25bca2bb2b3953f96642
Randy Rossi <randy.rossi@gmail.com>
2019-06-03 19:10:37 -0400

    Fix script

--- make_machines.sh
+++ make_machines.sh
@@ -6,19 +6,19 @@ cd ../..
 
 if [ "$1" = "pi2" ]
 then
-KERNEL = kernel8-32.img
+KERNEL=kernel8-32.img
 else
-KERNEL = kernel7.img
+KERNEL=kernel7.img
 fi
 
 make clean
 make -f Makefile-C64
-cp $KERNEL ${KERNEL}.c64
+echo cp $KERNEL ${KERNEL}.c64
 
 make clean
 make -f Makefile-C128
-cp $KERNEL ${KERNEL}.c128
+echo cp $KERNEL ${KERNEL}.c128
 
 make clean
 make -f Makefile-VIC20
-cp $KERNEL ${KERNEL}.vic20
+echo cp $KERNEL ${KERNEL}.vic20
